06d2834cd406b8462104d10a23c88cb553d79770
ryanmio <ryan@mioduski.us>
2024-09-15 09:50:02 -0400

    Initial Commit

---
+++ BonsaiTreeApp/BonsaiTreeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonsaiTreeAppApp.swift
+//  BonsaiTreeApp
+//
+//  Created by Ryan Mioduski iMac on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BonsaiTreeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BonsaiTreeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BonsaiTreeApp
+//
+//  Created by Ryan Mioduski iMac on 9/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
